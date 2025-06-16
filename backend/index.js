7bc9ea0c37f37d7c142cb87c77dbc72199073a28
Umar Farooq <umar53067@gmail.com>
2025-06-16 23:44:28 +0500

    intial commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express()
+
+
+app.listen(3000,()=>{
+    console.log("serer is running on port 400");
+    
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-gray-500 p-6  text-3xl text-white '>Tailwind CSS is configured</h1>
+    </>
+  )
+}
+
+export default App
